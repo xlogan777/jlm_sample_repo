6391c2e71b4085442f1bc55656b11afd584c589c
sammybaez1 <44147261+sammybaez1@users.noreply.github.com>
2020-01-14 19:47:51 -0500

    sample

---
+++ spring-practice/src/main/java/com/example/demo/SpringPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Define entry point for spring application
+public class SpringPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeApplication.class, args);
+	}
+
+}
